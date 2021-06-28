4a25c753606bc13d80be7b7e101cb46c30774b5a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-06-28 23:26:24 +0200

    Add Fontawesome

--- src/components/layout/navigation/NavBar/NavBar.js
+++ src/components/layout/navigation/NavBar/NavBar.js
@@ -1,7 +1,23 @@
+import 'bootstrap/scss/bootstrap.scss'
 
 const NavBar = () => {
     return (
-        <div>Nav bar</div>
+        <nav class="row d-none d-md-block">
+            <div class="col-sm p-2 d-flex justify-content-end col-gap bg-dark pe-3">
+                <a target="_blank"
+                rel="noopener noreferrer"
+                href="https://web.facebook.com/guylarocheg"
+                rel="noopener"
+                class="text-white ms-2"><i class="fab fa-facebook"></i></a>
+                <a target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/gullain_bisimwa"
+                rel="noopener"
+                class="text-white ms-2"><i class="fab fa-twitter"></i></a>
+                <a href="#" rel="noopener" class="text-white ms-2">English</a>
+                <a href="#" rel="noopener" class="text-white ms-2">Français</a>
+            </div>
+        </nav>
     );
 }
 
