e34f0792217ecf5b5c966c50fbc8ceefdef3b712
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-07-01 01:20:04 +0200

    Add modal

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import 'bootstrap/scss/bootstrap.scss';
+import { FaEye, FaSeedling } from 'react-icons/fa';
+
 import loan from './../../assets/Screenshot22.png';
 import ebola from './../../assets/Screenshot33.png';
 import ruby from './../../assets/Screenshot44.png';
@@ -7,6 +10,7 @@ import todo from './../../assets/Screenshot221.png';
 import resto from './../../assets/Screenshot222.png';
 import game from './../../assets/Screenshotgame.png';
 import festival from './../../assets/Screenshot.png';
+import { Modal, Button } from 'react-bootstrap';
 
 
 const Project = ({project}) => {
@@ -19,7 +23,11 @@ const Project = ({project}) => {
         {id:6, title:'DRC ebola evolution', link:'https://drc-ebola-outbreak.herokuapp.com/', key: 'python', src: ebola, stack: 'Flask, Postgresql, python3, dash, rest-api', details: "This project shows the evolution of the Ebola outbreak that has been occurring in the north-east of the DRC since August 2018, using dada science."},
         {id:7, title:'RUBY GEM SUGGESTIONS BOT', link:'https://github.com/guillainbisimwa/Twitter-gem-suggestions-bot/', key: 'ruby', src: ruby, stack: ' Ruby, twitter-bot, twitter-api, rspec, gitflow', details: "This is a Twitter bot that posts inspirational gem suggestions, reply to tweets, follow and tags ruby developer who needs help using our list of # (check githut)."},
     ];
-    const [state, setState] = useState(myProjects.filter(ob => ob.key == project))
+    const [state, setState] = useState(myProjects.filter(ob => ob.key == project));
+    const [open, setOpen] = useState(false);
+
+    const openModal = () => setOpen(true);
+    const closeModal = () => setOpen(false);
 
     return (
         <div className="border border-light p-4 img-container project">
@@ -31,16 +39,45 @@ const Project = ({project}) => {
                 <img id="projectTodDo" src={state[0].src} alt="Project Todo " />
             </a>
             <div className="info_project">
-                <p className="fw-bold text-truncate">To-Do-List {state[0].id}</p>
+                <p className="fw-bold text-truncate">{state[0].title}</p>
                
             </div>
            
             <div className="port-oly">
                 <div className="port-btn port-inner-btn">
-                    <button className="btn bg-primary_ btn-sm text-light mt-2 btn-block">live preview</button>
-                    <button className="btn bg-primary_ btn-sm text-light mt-2 btn-block">view details</button>
+                    <a className="btn bg-primary_ btn-sm text-light mt-2 btn-block" href={state[0].link}  target="_blank"
+                    rel="noopener noreferrer">Live preview <FaEye /></a>
+                    <button type="button" onClick={openModal} className="btn bg-primary_ btn-sm text-light mt-2 btn-block">View details <FaSeedling /></button>
                 </div>
             </div>
+            <Modal className='c-dark' size='lg' show={open} onHide={closeModal}>
+                <Modal.Header >
+                    <Modal.Title>{state[0].title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={state[0].src} className="img-fluid rounded-start" alt={state[0].title}/>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">{state[0].title}</h5>
+                                    <p className="card-text">{state[0].details}</p>
+                                    <p className="card-text"><small className="text-muted">{state[0].stack}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeModal}>
+                    Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
